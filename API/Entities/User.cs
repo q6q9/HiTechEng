0ffdaaeb46f16f69af20de5d467be5f9c4105b4f
evsvmx <evsv.mx@gmail.com>
2022-05-20 16:34:50 +0300

    Добавьте файлы проекта.

---
+++ API/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class User
+    {
+        [ScaffoldColumn(true)]
+        public int Id { get; set; }
+        [Required]
+        public string ?Name { get; set; }
+    }
+}
